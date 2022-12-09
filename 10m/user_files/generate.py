4e914a88522eea62ed2e03c996716d0647d1ecdc
Winston <31807289+WinstonJJFeng@users.noreply.github.com>
2022-12-09 13:39:28 -0800

    Update generate.py

--- 10m/user_files/generate.py
+++ 10m/user_files/generate.py
@@ -1,16 +1,16 @@
-file_path = "/Users/v.winston.feng/Downloads/bquxjob_58a3eb3d_184eef0eaa9.csv"
+file_path = "/Users/v.winston.feng/Downloads/recap_inventory_20221208.csv"
 
 def read_csv_to_pd(file_path):
     ## require csv file with both 'user_id' and 'drop_id' columns
     import pandas as pd
-    pd.read_csv(file_path)
-    return df[['user_id', 'drop_id']]
+    df = pd.read_csv(file_path)
+    return df[['user_id', 'drop_ids']]
 
 def generate_output_file(size):
     upload_df = read_csv_to_pd(file_path)
     import numpy as np
     for k,g in upload_df.groupby(np.arange(len(upload_df))//size):
-        g.to_csv('Export_{}.csv'.format(k+1), index=False)
+        g.to_csv('20221208_Export_{}.csv'.format(k+1), index=False)
 
-size = 30       
+size = 100000
 generate_output_file(size)
